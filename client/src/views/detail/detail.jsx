3e26b7775283167d32b93a99df41871101ee3692
Tatiana <tattmartinez@gmail.com>
2023-10-06 23:20:43 -0500

    making some styles to detail page

--- client/src/views/detail/detail.jsx
+++ client/src/views/detail/detail.jsx
@@ -2,13 +2,14 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getDogsById } from '../../redux/actions';
+import style from '../Detail/Detail.module.css';
 
 const Detail = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
   const allDogs = useSelector(state => state.allDogs)
-  console.log(allDogs)
+
 
   useEffect(() => {
   
@@ -16,10 +17,14 @@ const Detail = () => {
   }, [id]); 
 
   return (
-    <div>
-      <h1>Nombre</h1>
-      <h2>{allDogs.name}</h2>
-      <img src={allDogs.image} alt={allDogs.name} />
+    <div className={style.card} >
+      <img className={style.image} src={allDogs.image} alt='dogs name' />
+      <h1 className={style.name}>{allDogs.name}</h1>
+      <p className={style.temp}>ID: {allDogs.id}</p>
+      <p className={style.temp}>Height: {allDogs.height} cms</p>
+      <p className={style.temp}>Weight: {allDogs.weight} kg</p>
+      {/* <p> {allDogs.temperament}</p> */}
+      <p className={style.temp}>Life of Span: {allDogs.life_span}</p>
     </div>
   )
     
